884bddefc0fc26a59cf8ef89aeff682b4227f8fe
Sunbreak <sunbreak.wang@gmail.com>
2021-04-07 15:05:38 +0800

    Refactor iOS project to ObjC
    Signed-off-by: Sunbreak <sunbreak.wang@gmail.com>

--- ios/Classes/NativeInteropPlugin.m
+++ ios/Classes/NativeInteropPlugin.m
@@ -1,15 +1,20 @@
 #import "NativeInteropPlugin.h"
-#if __has_include(<native_interop/native_interop-Swift.h>)
-#import <native_interop/native_interop-Swift.h>
-#else
-// Support project import fallback if the generated compatibility header
-// is not copied when this plugin is created as a library.
-// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "native_interop-Swift.h"
-#endif
 
 @implementation NativeInteropPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftNativeInteropPlugin registerWithRegistrar:registrar];
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"native_interop"
+            binaryMessenger:[registrar messenger]];
+  NativeInteropPlugin* instance = [[NativeInteropPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
 }
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
 @end
